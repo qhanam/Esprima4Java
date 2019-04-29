40dc7d3fe00e9c797ca184ebb2035a479f554ac0
Quinn Hanam <qhanam@gmail.com>
2019-04-29 12:08:08 -0700

    configure as Maven project

---
+++ src/main/java/esprima4java/main/Esprima4Java.java
@@ -0,0 +1,9 @@
+package esprima4java.main;
+
+public class Esprima4Java {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/esprima4java/test/JsonToObjectTest.java
@@ -0,0 +1,14 @@
+package esprima4java.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JsonToObjectTest {
+
+    @Test
+    void test() {
+	fail("Not yet implemented");
+    }
+
+}
